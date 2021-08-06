d4e2f72e237ad2368b00b231c2497e718b8fbe34
demarijm <demarijmiller@gmail.com>
2021-08-06 12:49:30 -0400

    Added font

--- packages/my-first-theme/src/components/index.js
+++ packages/my-first-theme/src/components/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { connect } from "frontity"
+import { connect, Global, css } from "frontity"
 import Link from "@frontity/components/link"
 import Switch from "@frontity/components/switch"
 import List from "./list"
@@ -7,30 +7,36 @@ import Post from "./post"
 import Page from "./page"
 
 
-
 const Root = ({ state }) => {
   const data = state.source.get(state.router.link)
 
   return (
     <>
-      <h1>Hello Frontity</h1>
-      <p>Current URL: {state.router.link}</p>
-      <nav>
-        <Link link="/">Home</Link>
-        <br />
-        <Link link="/page/2">More posts</Link>
-        <br />
-        <Link link="/about-us">About Us</Link>
-      </nav>
-      <hr />
-      <main>
-        <Switch>
-          <List when={data.isArchive} />
-          <Post when={data.isPost}/>
-          <Page when={data.isPage}/>
-        </Switch>
-      </main>
-    </>
+         <Global
+            styles={css`
+                html {
+                    font-family: system-ui, Verdana, Aria;, sans-serif;
+                }
+            `}
+        />  
+         <h1>Hello Demari</h1>
+         <p>Current URL: {state.router.link}</p>
+          <nav>
+              <Link link="/">Home</Link>
+              <br />
+              <Link link="/page/2">More posts</Link>
+              <br />
+               <Link link="/about-us">About Us</Link>
+        </nav>
+            <hr />
+            <main>
+                <Switch>
+                <List when={data.isArchive} />
+                <Post when={data.isPost}/>
+                <Page when={data.isPage}/>
+                </Switch>
+            </main>
+            </>
   )
 }
 
